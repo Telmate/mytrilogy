5653ccb2d06f3fb7160193a94591aba621d3f10a
Grant Gongaware <grant@telmate.com>
2013-04-18 13:03:35 -0700

    support lol migrate to file mode

--- lib/mytrilogy/mysql_migrations.rb
+++ lib/mytrilogy/mysql_migrations.rb
@@ -3,6 +3,10 @@ module Mytrilogy
 
 class MysqlMigrations
 
+  class << self
+    attr_accessor :lol_dba_mode
+  end
+
   def self.migration_for_file(ruby_file_name)
     sqlfile = ruby_file_name[/(.*)\.rb$/,1] + ".sql"
     migration_sql_file(sqlfile)
@@ -10,6 +14,10 @@ class MysqlMigrations
 
   def self.migration_sql_file(sqlfile)
     puts "Running: #{File.basename(sqlfile)}"
+    if lol_dba_mode
+      puts `cat #{sqlfile} >> #{LolDba::Writer.path} 2>&1`
+      return
+    end
     raise "Missing mysql command line client." if `which mysql`.blank?
     config = ActiveRecord::Base.configurations[Rails.env]
     mysql_config_opts = "-h #{config['host']} -u #{config['username']} -p#{config['password']} #{config['database']}"
@@ -20,6 +28,8 @@ class MysqlMigrations
       raise "Problem with sql file."
     end
   end
+
+
 end
 
 end
